78b569870ac6a3753d120898c2a5824e7625cff8
delphine-nguyen <delphine.nguyend@gmail.com>
2024-02-21 10:33:04 +0100

    (feat) Add link on Home page to delete book from DB

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -14,12 +14,22 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class BookController extends AbstractController
 {
-    #[Route('/book/{id}', name: 'search_bookId')]
+    #[Route('/book/id/{id}', name: 'search_bookId')]
     public function bookById(BookRepository $bookRepository, int $id): Response
     {
         $book = $bookRepository->find($id);
         return $this->render('book/search_bookId.html.twig', [
             "result" => $book,
+        ]);
+    }
+
+    #[Route('/book/title/{title}', name: 'search_bookTitle')]
+    public function bookByTitle(BookRepository $bookRepository, string $title): Response
+    {
+        $books = $bookRepository->findByTitle($title);
+        return $this->render('book/search_bookTitle.html.twig', [
+            "results" => $books,
+            "searchTitle" => $title
         ]);
     }
 
@@ -38,5 +48,16 @@ class BookController extends AbstractController
         return $this->render("book/create_book.html.twig", [
             "form" => $form->createView(),
         ]);
+    }
+
+    #[Route("/delete/book/{id}", name: "delete_book")]
+    public function deleteBook(BookRepository $bookRepository, EntityManagerInterface $manager, int $id): Response
+    {
+        $book = $bookRepository->find($id);
+        if ($book) {
+            $manager->remove($book);
+            $manager->flush();
+        }
+        return $this->redirectToRoute("app_home");
     }
 }
