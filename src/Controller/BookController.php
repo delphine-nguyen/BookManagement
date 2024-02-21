5ce35f13667632bc3399b07ce402f645d393ad2c
delphine-nguyen <delphine.nguyend@gmail.com>
2024-02-21 09:53:03 +0100

    (feat) Implement form to create new Book entities

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Form\BookType;
+
 use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,6 +20,23 @@ class BookController extends AbstractController
         $book = $bookRepository->find($id);
         return $this->render('book/search_bookId.html.twig', [
             "result" => $book,
+        ]);
+    }
+
+    #[Route("/create/book", name: "create_book", methods: ["GET", "POST"])]
+    public function createBook(Request $request, EntityManagerInterface $manager): Response
+    {
+        $book = new Book();
+        $form = $this->createForm(BookType::class, $book);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($book);
+            $manager->flush();
+            return $this->redirectToRoute("app_home");
+        }
+        return $this->render("book/create_book.html.twig", [
+            "form" => $form->createView(),
         ]);
     }
 }
