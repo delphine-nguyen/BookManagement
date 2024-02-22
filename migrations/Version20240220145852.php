7d94dcd6e5dd1ad3021a7f6fa1ff8103039a586f
delphine-nguyen <delphine.nguyend@gmail.com>
2024-02-22 09:04:30 +0100

    (refactor) Make SQL more lisible

--- migrations/Version20240220145852.php
+++ migrations/Version20240220145852.php
@@ -20,7 +20,19 @@ final class Version20240220145852 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE book (id INT AUTO_INCREMENT NOT NULL, isbn VARCHAR(15) NOT NULL, title VARCHAR(255) NOT NULL, summary LONGTEXT NOT NULL, description LONGTEXT NOT NULL, price DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql(
+            'CREATE TABLE book 
+                (
+                    id INT AUTO_INCREMENT NOT NULL, 
+                    isbn VARCHAR(15) NOT NULL, 
+                    title VARCHAR(255) NOT NULL, 
+                    summary LONGTEXT NOT NULL, 
+                    description LONGTEXT NOT NULL, 
+                    price DOUBLE PRECISION NOT NULL, 
+                    PRIMARY KEY(id)
+                ) 
+                DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB'
+        );
     }
 
     public function down(Schema $schema): void
--- migrations/Version20240221150728.php
+++ migrations/Version20240221150728.php
@@ -20,7 +20,16 @@ final class Version20240221150728 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE editor (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(50) NOT NULL, address VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql(
+            'CREATE TABLE editor 
+                        (
+                            id INT AUTO_INCREMENT NOT NULL, 
+                            name VARCHAR(50) NOT NULL, 
+                            address VARCHAR(255) NOT NULL, 
+                            PRIMARY KEY(id)
+                        ) 
+                        DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB'
+        );
         $this->addSql('DROP TABLE user');
     }
 
